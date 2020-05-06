6db0323891b5dab629caee9a45132acf5b1569b1
nikita1998ivanov <61649226+nikita1998ivanov@users.noreply.github.com>
2020-05-06 11:10:44 +0300

    Add files via upload

---
+++ 1_3.py
@@ -0,0 +1,7 @@
+my_list = [11, 2, 32, 4 , 5, 16, 7, 8, 9, 10]
+sum = 0
+for i in my_list:
+    if(i > 10):
+        sum += i
+        
+print('sum = ', sum)
---
+++ 1_4.py
@@ -0,0 +1,2 @@
+my_list = [11, 2, 32, 4 , 5, 16, 7, 8, 9, 10]
+print('max = ', max(my_list))
