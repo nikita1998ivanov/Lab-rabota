1a6a35f7aec10046fcbe9ccb77fe256a148aa558
nikita1998ivanov <61649226+nikita1998ivanov@users.noreply.github.com>
2020-05-06 11:47:32 +0300

    Add files via upload

---
+++ 7_3.py
@@ -0,0 +1,37 @@
+d = {}
+with open("students.csv") as f:
+    next(f)
+    for line in f:
+        h, nm, a, db = line.split(";")
+        d.setdefault(int(h), []).append((nm, int(a), db))
+l = d.values()
+l = list(l)
+l.sort()
+print(l)
+
+def t():
+    count = 0
+    while True:
+        print("Для увеличения возраста всех студентов, введите: age++")
+        print("Чтобы выйти, введите: quit")
+
+        command = input()
+
+        if command == "age++":
+            count += 1
+            for i in range(len(l)):
+                for j in range(len(l[i])):
+                    for k in range(len(l[i][j])):
+                        if(k == 1):
+                            print(l[i][j][k] + count)
+                        else:
+                            print(l[i][j][k])
+
+        elif command == "quit":
+            break
+
+        else:
+            print("Команда не верна.\n")
+
+
+t()
